c28d5fcd8902b0bfebbcdc98927e5dfb75638dbf
Liam Evans <liamevans1818@gmail.com>
2022-11-22 15:36:11 -0500

    added no click to the array-size when running

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -329,6 +329,8 @@ export function Footer() {
                 buttons[i].classList.remove("no-click");
             }
         }
+        let sizeButton:any = $(".array-size-slider")
+        sizeButton.classList.remove("no-click");
     }
 
     /*Disable the correct buttons and all buttons to no-click*/
@@ -351,6 +353,8 @@ export function Footer() {
         for(let i = 0; i < buttons.length; i++){
             buttons[i].classList.add("no-click");
         }
+        let sizeButton:any = $(".array-size-slider")
+        sizeButton.classList.add("no-click");
     }
 
     /*Check if the sorting has been stopped*/
